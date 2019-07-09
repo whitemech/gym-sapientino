5702b87ec340aa235a7881f2b00b7e003f87ea09
MarcoFavorito <marco.favorito@gmail.com>
2019-07-09 13:22:01 +0200

    set up project structure.

---
+++ gym_sapientino/__version__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'gym_sapientino'
+__description__ = 'Gym Sapientino environment using Pygame'
+__url__ = 'https://github.com/sapienza-rl/gym-sapientino.git'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Marco Favorito, Luca Iocchi'
+__author_email__ = 'favorito@diag.uniroma1.it, iocchi@diag.uniroma1.it'
+__license__ = 'MIT License'
+__copyright__ = '2019 Marco Favorito, Luca Iocchi'
