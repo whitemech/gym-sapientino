b7b3cb00dd808974bd8ed09d6307e02c0208da61
Roberto Cipollone <cipollone.rt@gmail.com>
2021-07-12 15:11:22 +0200

    Moved tests to tests/src because I'm adding resources.

---
+++ tests/src/test_observations.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019-2020 Marco Favorito, Luca Iocchi
+#
+# ------------------------------
+#
+# This file is part of gym-sapientino.
+#
+# gym-sapientino is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# gym-sapientino is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gym-sapientino.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Tests for the Sapientino Gym environment."""
+
+import logging
+
+import pytest
+
+from gym_sapientino import SapientinoDictSpace, __version__, actions, observations
+from gym_sapientino.core.configurations import SapientinoAgentConfiguration
+
+
+def test_version():
+    """Test version."""
+    assert __version__ == "0.2.1"
+
+
+@pytest.fixture
+def sapientino_dict(n_agents: int) -> SapientinoDictSpace:
+    """Create a sapientino instance with n agents."""
+    return None
